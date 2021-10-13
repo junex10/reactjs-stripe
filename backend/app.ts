b00fa663b86d0503c149c12f4c38ab8b5e50dbb2
Jose Herrada <jherrada@wcloudservices.com>
2021-10-13 19:01:58 -0400

    Agregado cors

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 import { Services } from './services/Services';
 
@@ -21,6 +22,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(cors({
+  origin: '*'
+}))
 
 app.use('/api-docs', swaggerUi.serve);
 app.get('/api-docs', swaggerUi.setup(swaggerDocument));
