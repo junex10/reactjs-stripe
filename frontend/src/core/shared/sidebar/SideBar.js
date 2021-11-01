b29987424baa1629bcb5348523d8ddd8a8a8ced8
Jose Herrada <joaealejunior@gmail.com>
2021-11-01 14:24:02 -0400

    iconos sidebar

--- frontend/src/core/shared/sidebar/SideBar.js
+++ frontend/src/core/shared/sidebar/SideBar.js
@@ -14,13 +14,6 @@ class SideBar extends Component {
                 </a>
 
                 <hr className="sidebar-divider my-0" />
-
-                {/*<li className="nav-item active">
-                    <a className="nav-link" href="index.html">
-                        <i className="fas fa-fw fa-tachometer-alt"></i>
-                        <span>Dashboard</span></a>
-        </li>*/}
-
                 <hr className="sidebar-divider" />
 
                 <div className="sidebar-heading">
