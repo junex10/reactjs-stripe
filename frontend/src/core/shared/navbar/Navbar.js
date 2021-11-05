60e24ea023e5b4fc863d56bb18861f6569fcff02
Jose Herrada <joaealejunior@gmail.com>
2021-11-04 20:01:45 -0400

    Navbar store

--- frontend/src/core/shared/navbar/Navbar.js
+++ frontend/src/core/shared/navbar/Navbar.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import userSVG from './../../../img/dashboard/undraw_profile.svg';
+
 import DashboardNavbar from './DashboardNavbar';
+import { userSession } from './../../../commons/config';
 
 class Navbar extends Component {
     constructor(props) {
         super(props);
         this.background = props.background;
         this.navbar = props.site;
+
+        this.email = userSession !== null ? userSession.user : '';
     }
     SiteNavbar = () => {
         return (<header id="header" className="fixed-top d-flex align-items-center header-transparent" style={this.background}>
@@ -20,10 +25,29 @@ class Navbar extends Component {
 
                 <nav id="navbar" className="navbar">
                     <ul>
+                        {
+                            userSession !== null ?
+                                <li className="nav-link scrollto limitText">
+                                    <img className="mr-4 img-profile rounded-circle"
+                                        src={userSVG} width='30' alt='userImage' />
+                                    <span className='text-white'>{this.email}</span>
+                                </li>
+                                : ''
+                        }
                         <li className="nav-link scrollto"><Link to='/home'>Home</Link></li>
                         <li className="nav-link scrollto"><Link to='/tienda'>Tienda</Link></li>
-                        <li className="nav-link scrollto"><Link to='/login'>Log in</Link></li>
-                        <li className="nav-link scrollto"><Link to='/login/signup'><button className='btn btn-success'>Sign Up</button></Link></li>
+                        {
+                            userSession === null ?
+                                <>
+                                    <li className="nav-link scrollto"><Link to='/login'>Log in</Link></li>
+                                    <li className="nav-link scrollto"><Link to='/login/signup'><button className='btn btn-success'>Sign Up</button></Link></li>
+                                </>
+                            : <>
+                                <li className='nav-link scrollto'>
+                                    <Link to='/dashboard/user/profile'>Perfil</Link>
+                                </li>
+                            </>
+                        }
                     </ul>
                     <i className="bi bi-list mobile-nav-toggle"></i>
                 </nav>
